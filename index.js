0977dd291c9a5cdeb489007986f071802a31218e
Daniel Lamberti <daniellamberti@gmail.com>
2024-06-29 23:47:45 -0300

    first commmit

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const viewMoreBtn = document.getElementById("view-more");
+const viewLessBtn = document.getElementById("view-less");
+const articlesHiddenSectionEl = document.getElementById("articles-hidden");
+const hambPic = document.getElementById("hamb-pic");
+const hambMenu = document.getElementById("hamb-menu");
+
+console.log("Hola");
+
+viewMoreBtn.addEventListener("click", function () {
+   articlesHiddenSectionEl.style.display="flex";
+   viewLessBtn.style.visibility="visible"; 
+   viewMoreBtn.style.visibility="hidden"; 
+});
+
+viewLessBtn.addEventListener("click", function () {
+    articlesHiddenSectionEl.style.display="none"; 
+    document.getElementById("start"). scrollIntoView({ behavior: "smooth" });
+    viewLessBtn.style.visibility="hidden";
+    viewMoreBtn.style.visibility="visible";
+ });
+
+ hambPic.addEventListener("click", function () {
+   console.log("Button Clicked");
+if(hambMenu.style.display === "none" || hambMenu.style.display === "") {
+   hambMenu.style.display="flex";
+  } else if (hambMenu.style.display==="flex") {
+   hambMenu.style.display="none";
+  }
+});
+
+})
