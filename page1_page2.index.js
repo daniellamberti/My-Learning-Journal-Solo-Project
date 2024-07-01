0c297a1796d4a88d9a550585f48cb9c5a5cefb44
Daniel Lamberti <daniellamberti@gmail.com>
2024-07-01 20:29:32 -0300

    page1_page2.js modification

--- page1_page2.index.js
+++ page1_page2.index.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+   
 const hambPic = document.getElementById("hamb-pic");
 const hambMenu = document.getElementById("hamb-menu");
 
-
 hambPic.addEventListener("click", function () {
-    console.log("Button Clicked");
  if(hambMenu.style.display === "none" || hambMenu.style.display === "") {
     hambMenu.style.display="flex";
    } else if (hambMenu.style.display==="flex") {
